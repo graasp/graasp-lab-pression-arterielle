35c9674319071ba18ab3688b7d36e4983ac695c7
Mashkour <azizmashkour@gmail.com>
2018-10-24 12:11:03 +0200

    feat: create english and french translation files

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App } from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const props = {
+    t: jest.fn(),
+  };
+
+  ReactDOM.render(<App {...props} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
