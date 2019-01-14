b1347d8d52f45c691019b076d257980318103cce
Mashkour <azizmashkour@gmail.com>
2019-01-14 18:52:06 +0100

    fix: separate app state into a config file

---
+++ src/config/AppState.js
@@ -0,0 +1,14 @@
+export const AppState = {
+  showCarotidNerve: false,
+  showHeringNerve: false,
+  showParaNerve: false,
+  showBulbNerve: false,
+  showCordNerve: false,
+  showSympaNerve: false,
+  applySection: false,
+  heartBeat: false,
+  pressure: false,
+  obserViewActive: true,
+};
+
+export default AppState;
