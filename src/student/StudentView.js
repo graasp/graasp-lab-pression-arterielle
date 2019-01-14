feca5c4b293f90eaaaa887096475755500a76443
Mashkour <azizmashkour@gmail.com>
2019-01-14 18:50:09 +0100

    feat: make sure we handle all organs hover events

--- src/student/StudentView.js
+++ src/student/StudentView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MainView from '../components/MainView';
+import { AppState } from '../config/AppState';
 
 class StudentView extends Component {
   static propTypes = {
@@ -9,13 +10,10 @@ class StudentView extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      applySection: false,
-      heartBeat: false,
-      pressure: false,
-    };
     this.handleSection = this.handleSection.bind(this);
   }
+
+  state = AppState;
 
   handleSection = () => {
     const {
@@ -30,14 +28,54 @@ class StudentView extends Component {
     });
   };
 
+  handleCarotidHover = () => {
+    const { showCarotidNerve } = this.state;
+    this.setState({ showCarotidNerve: !showCarotidNerve });
+  }
+
+  handleHeringHover = () => {
+    const { showHeringNerve } = this.state;
+    this.setState({ showHeringNerve: !showHeringNerve });
+  }
+
+  handleParaHover = () => {
+    const { showParaNerve } = this.state;
+    this.setState({ showParaNerve: !showParaNerve });
+  }
+
+  handleBulbHover = () => {
+    const { showBulbNerve } = this.state;
+    this.setState({ showBulbNerve: !showBulbNerve });
+  }
+
+  handleCordHover = () => {
+    const { showCordNerve } = this.state;
+    this.setState({ showCordNerve: !showCordNerve });
+  }
+
+  handleSympaHover = () => {
+    const { showSympaNerve } = this.state;
+    this.setState({ showSympaNerve: !showSympaNerve });
+  }
+
   render() {
     const {
       applySection,
       heartBeat,
       pressure,
+      themeColor,
+      obserViewActive,
+      showCarotidNerve,
+      showHeringNerve,
+      showBulbNerve,
+      showParaNerve,
+      showCordNerve,
+      showSympaNerve,
     } = this.state;
 
     const { t } = this.props;
+
+    const defaultColor = themeColor || '#0f94f8';
 
     return (
       <div className="App">
@@ -47,6 +85,20 @@ class StudentView extends Component {
           heartBeat={heartBeat}
           handleSection={this.handleSection}
           t={t}
+          defaultColor={defaultColor}
+          obserViewActive={obserViewActive}
+          handleCarotidHover={this.handleCarotidHover}
+          handleHeringHover={this.handleHeringHover}
+          handleParaHover={this.handleParaHover}
+          handleBulbHover={this.handleBulbHover}
+          handleCordHover={this.handleCordHover}
+          handleSympaHover={this.handleSympaHover}
+          showCarotidNerve={showCarotidNerve}
+          showHeringNerve={showHeringNerve}
+          showBulbNerve={showBulbNerve}
+          showParaNerve={showParaNerve}
+          showCordNerve={showCordNerve}
+          showSympaNerve={showSympaNerve}
         />
       </div>
     );
