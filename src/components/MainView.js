2dab746c33cec10efd88d17dfe5df4a145cf86b1
Mashkour <azizmashkour@gmail.com>
2019-01-14 18:51:12 +0100

    fix: pass updated props to child components

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Row } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 import ImageManager from './ImageManager';
+import Visualizer from './Visualizer';
 import './MainView.css';
 
 const MainView = ({
@@ -9,25 +10,81 @@ const MainView = ({
   handleSection,
   pressure,
   applySection,
+  defaultColor,
+  obserViewActive,
+  handleCarotidHover,
+  handleHeringHover,
+  handleParaHover,
+  handleBulbHover,
+  handleCordHover,
+  handleSympaHover,
+  showCarotidNerve,
+  showHeringNerve,
+  showBulbNerve,
+  showParaNerve,
+  showCordNerve,
+  showSympaNerve,
   t,
 }) => (
-  <Row className="Main">
-    <ImageManager
-      applySection={applySection}
-      t={t}
-      heartBeat={heartBeat}
-      pressure={pressure}
-      handleSection={handleSection}
-    />
-  </Row>
+  <div className="Main-container">
+    <Row>
+      <Col md={12}>
+        <h1 className="lab-title" style={{ backgroundColor: defaultColor }}>{t('Synchronization of the ovarian cycle')}</h1>
+      </Col>
+    </Row>
+    <Row className="Main">
+      <Col sm="8">
+        <ImageManager
+          applySection={applySection}
+          t={t}
+          heartBeat={heartBeat}
+          pressure={pressure}
+          handleSection={handleSection}
+          handleCarotidHover={handleCarotidHover}
+          handleHeringHover={handleHeringHover}
+          handleParaHover={handleParaHover}
+          handleBulbHover={handleBulbHover}
+          handleCordHover={handleCordHover}
+          handleSympaHover={handleSympaHover}
+          showCarotidNerve={showCarotidNerve}
+          showHeringNerve={showHeringNerve}
+          showBulbNerve={showBulbNerve}
+          showParaNerve={showParaNerve}
+          showCordNerve={showCordNerve}
+          showSympaNerve={showSympaNerve}
+        />
+      </Col>
+      <Col sm="4">
+        <Visualizer
+          themeColor={defaultColor}
+          t={t}
+          obserViewActive={obserViewActive}
+        />
+      </Col>
+    </Row>
+  </div>
 );
 
 MainView.propTypes = {
   applySection: PropTypes.bool.isRequired,
+  defaultColor: PropTypes.string.isRequired,
+  obserViewActive: PropTypes.bool.isRequired,
   handleSection: PropTypes.func.isRequired,
   heartBeat: PropTypes.bool.isRequired,
   pressure: PropTypes.bool.isRequired,
+  showCarotidNerve: PropTypes.bool.isRequired,
+  showHeringNerve: PropTypes.bool.isRequired,
+  showBulbNerve: PropTypes.bool.isRequired,
+  showParaNerve: PropTypes.bool.isRequired,
+  showCordNerve: PropTypes.bool.isRequired,
+  showSympaNerve: PropTypes.bool.isRequired,
   t: PropTypes.func.isRequired,
+  handleCarotidHover: PropTypes.func.isRequired,
+  handleHeringHover: PropTypes.func.isRequired,
+  handleParaHover: PropTypes.func.isRequired,
+  handleBulbHover: PropTypes.func.isRequired,
+  handleCordHover: PropTypes.func.isRequired,
+  handleSympaHover: PropTypes.func.isRequired,
 };
 
 export default MainView;
