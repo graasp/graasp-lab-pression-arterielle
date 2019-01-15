755350b4fc3f13d0f7d0ac1b209d268b05758c8f
Mashkour <azizmashkour@gmail.com>
2019-01-15 15:42:25 +0100

    feat: configure redux middle ware and store

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { I18nextProvider } from 'react-i18next';
-import { createStore } from 'redux';
-import reducers from './reducers';
-import i18n from './i18n';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Root from './Root';
+import * as registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <Provider store={createStore(reducers, {})}>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-  </Provider>,
-  document.getElementById('root'),
-);
-registerServiceWorker();
+ReactDOM.render(<Root />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+registerServiceWorker.unregister();
