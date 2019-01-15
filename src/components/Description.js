2ad860df79d55d166b3f6779fc6a90738accb827
Mashkour <azizmashkour@gmail.com>
2019-01-15 16:19:17 +0100

    feat: add lab title to it config panel component

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,11 +1,18 @@
-import { Button } from 'reactstrap';
 import React from 'react';
 import { connect } from 'react-redux';
+import Switch from 'react-switch';
 import PropTypes from 'prop-types';
+import {
+  Button,
+  Col,
+  Row,
+} from 'reactstrap';
 
 const Description = ({
   t,
   themeColor,
+  handleTitle,
+  showTitle,
 }) => (
   <div className="Description-container">
     <h2>{t('Description')}</h2>
@@ -24,12 +31,26 @@ const Description = ({
       :
       57
     </Button>
+    <Row className="title-switch">
+      <Col xs="8">
+        <h5 className="display-title">{t('Display Lab title')}</h5>
+      </Col>
+      <Col xs="4">
+        <Switch
+          onChange={handleTitle}
+          checked={showTitle}
+          id="title-switch"
+        />
+      </Col>
+    </Row>
   </div>
 );
 
 Description.propTypes = {
   t: PropTypes.func.isRequired,
   themeColor: PropTypes.string.isRequired,
+  handleTitle: PropTypes.func.isRequired,
+  showTitle: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => ({
