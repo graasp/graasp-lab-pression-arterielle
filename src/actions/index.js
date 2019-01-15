7fa9a726b564de6b5cfeabdeaef60d1086346898
Mashkour <azizmashkour@gmail.com>
2019-01-15 15:51:00 +0100

    feat: create default redux action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
-export * from './settings';
+import { THEME_COLOR, SET_LANGUAGE } from './types';
+
+export function themeColor({ newColor }) {
+  return {
+    type: THEME_COLOR,
+    payload: newColor,
+  };
+}
+
+export function defaultLang({ newLang }) {
+  return {
+    type: SET_LANGUAGE,
+    payload: newLang,
+  };
+}
