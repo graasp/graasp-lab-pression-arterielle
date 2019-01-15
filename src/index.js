27e83c33ba1a7cd93d3aa21f920d96f5d6835642
Mashkour <azizmashkour@gmail.com>
2019-01-15 12:03:18 +0100

    feat: connect reducers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
+import { createStore } from 'redux';
+import reducers from './reducers';
 import i18n from './i18n';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
@@ -8,9 +11,11 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <I18nextProvider i18n={i18n}>
-    <App />
-  </I18nextProvider>,
+  <Provider store={createStore(reducers, {})}>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </Provider>,
   document.getElementById('root'),
 );
 registerServiceWorker();
