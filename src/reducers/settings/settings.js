61ed768fefecdf33ce8819a52cf08001bcb893ba
Mashkour <azizmashkour@gmail.com>
2019-01-15 12:05:44 +0100

    feat: create and config default reducers

---
+++ src/reducers/settings/settings.js
@@ -0,0 +1,33 @@
+import {
+  SET_CONFIG,
+  SET_LANGUAGE,
+  SET_HEADER_BACKGROUND_COLOR,
+} from '../../types';
+
+const INITIAL_STATE = {
+  changedLanguage: 'en',
+  header_background_color: '#2196F5',
+};
+
+export default (state = INITIAL_STATE, { type, payload }) => {
+  console.log('Getting new language', payload);
+  switch (type) {
+    case SET_LANGUAGE:
+      return {
+        ...state,
+        changedLanguage: payload,
+      };
+    case SET_HEADER_BACKGROUND_COLOR:
+      return {
+        ...state,
+        headerBackgroundColor: payload,
+      };
+    case SET_CONFIG:
+      return {
+        ...state,
+        ...payload,
+      };
+    default:
+      return state;
+  }
+};
