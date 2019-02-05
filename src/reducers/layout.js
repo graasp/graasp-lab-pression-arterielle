78f5f4199ba71db01a0f44de9194b1dda67f9cdb
Mashkour <azizmashkour@gmail.com>
2019-02-05 17:39:29 +0100

    fix: config reducers and action types

---
+++ src/reducers/layout.js
@@ -0,0 +1,40 @@
+import {
+  CHANGE_THEME_COLOR,
+  TOGGLE_HEADER,
+  TOGGLE_SIDE_MENU,
+  TOGGLE_NERVE,
+} from '../types';
+
+const INITIAL_STATE = {
+  themeColor: 'rgb(0, 150, 136)',
+  showHeader: true,
+  showSideMenu: false,
+  showNerve: false,
+};
+
+export default (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+    case CHANGE_THEME_COLOR:
+      return {
+        ...state,
+        themeColor: payload,
+      };
+    case TOGGLE_HEADER:
+      return {
+        ...state,
+        showHeader: payload,
+      };
+    case TOGGLE_SIDE_MENU:
+      return {
+        ...state,
+        showSideMenu: payload,
+      };
+    case TOGGLE_NERVE:
+      return {
+        ...state,
+        showNerve: payload,
+      };
+    default:
+      return state;
+  }
+};
