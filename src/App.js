f30e190942dee0639e3f4a8ad114ede2ac173acd
Mashkour <azizmashkour@gmail.com>
2019-02-05 17:35:29 +0100

    fix: refactor app component and update new components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import Qs from 'qs';
 import PropTypes from 'prop-types';
 import { withNamespaces } from 'react-i18next';
-import TeacherView from './teacher/TeacherView';
-import StudentView from './student/StudentView';
+import TeacherMode from './modes/TeacherMode';
+import StudentMode from './modes/StudentMode';
 
 export class App extends Component {
   static propTypes = {
     i18n: PropTypes.shape({}).isRequired,
-    t: PropTypes.func.isRequired,
   }
 
   constructor(props) {
@@ -24,18 +23,17 @@ export class App extends Component {
 
   render() {
     const { mode } = this.state;
-    const { t } = this.props;
 
     switch (mode) {
       // show teacher view when in teacher mode
       case 'teacher':
         // TODO::the teacher view is empty for the moment
-        return <TeacherView />;
+        return <TeacherMode />;
 
       // by default go with the student mode
       case 'student':
       default:
-        return <StudentView t={t} />;
+        return <StudentMode />;
     }
   }
 }
