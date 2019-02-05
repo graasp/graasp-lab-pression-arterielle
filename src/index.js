1aeea1bab43eeca6bd9f3314340148bff188e3b5
Mashkour <azizmashkour@gmail.com>
2019-02-05 17:35:54 +0100

    chore: config and mount redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import Root from './Root';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import Root from './Root';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/configureStore';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const root = document.getElementById('root');
+const { store } = configureStore();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(
+  <Provider store={store}>
+    <Root />
+  </Provider>,
+  root,
+);
+
 registerServiceWorker();
