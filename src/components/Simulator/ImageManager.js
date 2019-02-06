863cbaf50180c5323819fa6dc51033faab8a945b
azizmashkour <azizmashkour@gmail.com>
2019-02-06 11:33:45 +0100

    fix: update variables names and remove unnecessary ones

--- src/components/Simulator/ImageManager.js
+++ src/components/Simulator/ImageManager.js
@@ -83,7 +83,7 @@ export class ImageManager extends Component {
 
   render() {
     const {
-      anchorEl,
+      anchorElCarotid,
       anchorElHeart,
       anchorElSympatetic,
       anchorElParaSympa,
@@ -91,7 +91,7 @@ export class ImageManager extends Component {
       anchorElBulb,
       anchorElSpinal,
     } = this.state;
-    const open = Boolean(anchorEl);
+    const open = Boolean(anchorElCarotid);
     const openHeart = Boolean(anchorElHeart);
     const openSympatetic = Boolean(anchorElSympatetic);
     const openParaSympa = Boolean(anchorElParaSympa);
@@ -100,10 +100,8 @@ export class ImageManager extends Component {
     const openSpinal = Boolean(anchorElSpinal);
     const {
       classes,
-      heartBeat,
-      pressure,
-      handleSection,
       showNerve,
+      handleToggleNerveSection,
       t,
     } = this.props;
 
@@ -116,7 +114,7 @@ export class ImageManager extends Component {
             paper: classes.paper,
           }}
           open={open}
-          anchorEl={anchorEl}
+          anchorEl={anchorElCarotid}
           anchorOrigin={{
             vertical: 'top',
             horizontal: 'left',
@@ -331,7 +329,7 @@ export class ImageManager extends Component {
                   d="M204.390692,140.837642 C193.794802,130.413726 178.036316,132.288692 168.310526,143.873301 L164.50142,148.404468 L160.692314,143.873301 C150.98586,132.288692 135.208038,130.413726 124.612148,140.837642 C112.469413,152.801709 111.83134,174.274531 122.697927,187.243043 L160.112247,231.840441 C162.529192,234.719853 166.454312,234.719853 168.871258,231.840441 L206.285577,187.243043 C217.1715,174.274531 216.533427,152.801709 204.390692,140.837642 Z"
                   id="heart"
                   fillRule="nonzero"
-                  className={heartBeat ? 'heart heart-enable' : 'heart'}
+                  className={showNerve ? 'heart heart-enable' : 'heart'}
                   aria-owns={open ? 'heart-over-popover' : undefined}
                   aria-haspopup="true"
                   onMouseEnter={this.handleHeartPopoverOpen}
@@ -431,7 +429,7 @@ export class ImageManager extends Component {
               </g>
 
               <g id="nerfs" transform="translate(367.000000, 164.000000)" stroke="#000000" strokeLinecap="square">
-                { pressure ? (
+                { showNerve ? (
                   <path
                     d="M0,354.5 C58.3611069,326.381281 102.220033,312.554262 131.57678,313.01894 C139.172562,313.139171 145.797472,314.216185 151.451508,316.24998 C165.507964,321.306183 195.627428,329.546195 230.667286,338.064814 C293.531219,353.347807 372.23235,369.527545 402.427182,369.827807 C405.337815,369.856751 402.427182,369.360181 411.717364,369.827807 C495.487829,374.044437 536.117961,330.935168 533.607759,240.5"
                     id="Nerf-bottom-dashed"
@@ -442,7 +440,7 @@ export class ImageManager extends Component {
                     <path
                       d="M0,354.5 C73.4614954,319.105827 123.945331,306.355821 151.451508,316.24998 C190.726221,330.377361 355.40199,369.360181 402.427182,369.827807 C405.337815,369.856751 402.427182,369.360181 411.717364,369.827807 C495.487829,374.044437 536.117961,330.935168 533.607759,240.5"
                       id="Nerf-bottom"
-                      className={pressure ? 'pressure pressure-enable-nerf-bottom' : 'pressure'}
+                      className={showNerve ? 'pressure pressure-enable-nerf-bottom' : 'pressure'}
                       aria-owns={open ? 'heart-over-popover' : undefined}
                       aria-haspopup="true"
                       onMouseEnter={this.handleSympateticPopoverOpen}
@@ -471,7 +469,7 @@ export class ImageManager extends Component {
                 <path
                   d="M34.1370209,35 L54.5681617,8.9953125 C55.1439461,8.2625 55.1439461,7.075 54.5681617,6.34375 C50.5413535,1.21875 44.0112947,1.21875 39.9844865,6.34375 L25.8034922,24.39375 L22.7514662,20.509375 C23.2805987,18.803125 23.5715601,16.9453125 23.5715601,15 C23.5715601,6.715625 18.2949682,0 11.7857801,0 C5.27659195,0 1.74464592e-15,6.715625 1.74464592e-15,15 C1.74464592e-15,23.284375 5.27659195,30 11.7857801,30 C12.3431492,30 12.8894692,29.95 13.4259678,29.8546875 L17.4687359,35 L13.4259678,40.1453125 C12.8894692,40.05 12.3419216,40 11.7857801,40 C5.27659195,40 0,46.715625 0,55 C0,63.284375 5.27659195,70 11.7857801,70 C18.2949682,70 23.5715601,63.284375 23.5715601,55 C23.5715601,53.0546875 23.2805987,51.196875 22.7514662,49.490625 L25.8034922,45.60625 L39.9844865,63.65625 C44.0112947,68.78125 50.5413535,68.78125 54.5681617,63.65625 C55.1439461,62.925 55.1439461,61.7375 54.5681617,61.0046875 L34.1370209,35 Z M11.7857801,20 C9.62014297,20 7.8571867,17.75625 7.8571867,15 C7.8571867,12.24375 9.62014297,10 11.7857801,10 C13.9514171,10 15.7143734,12.24375 15.7143734,15 C15.7143734,17.75625 13.9514171,20 11.7857801,20 Z M11.7857801,60 C9.62014297,60 7.8571867,57.75625 7.8571867,55 C7.8571867,52.24375 9.62014297,50 11.7857801,50 C13.9514171,50 15.7143734,52.24375 15.7143734,55 C15.7143734,57.75625 13.9514171,60 11.7857801,60 Z"
                   id="Shape"
-                  onClick={handleSection}
+                  onClick={handleToggleNerveSection}
                   className={showNerve ? 'scissors scissors-enable' : 'scissors'}
                 />
               </g>
@@ -484,10 +482,8 @@ export class ImageManager extends Component {
 }
 
 ImageManager.propTypes = {
-  handleSection: PropTypes.func.isRequired,
+  handleToggleNerveSection: PropTypes.func.isRequired,
   classes: PropTypes.shape({}).isRequired,
-  heartBeat: PropTypes.bool.isRequired,
-  pressure: PropTypes.bool.isRequired,
   showNerve: PropTypes.bool.isRequired,
   t: PropTypes.func.isRequired,
 };
