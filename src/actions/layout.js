c2b62687f084f376ca83b0d1316b72cf780621fd
azizmashkour <azizmashkour@gmail.com>
2019-02-06 12:19:59 +0100

    feat: create and configure new action creators

--- src/actions/layout.js
+++ src/actions/layout.js
@@ -1,6 +1,7 @@
 import {
   TOGGLE_SIDE_MENU,
   TOGGLE_HEADER,
+  TOGGLE_LOADER,
   TOGGLE_NERVE,
   CHANGE_THEME_COLOR,
 } from '../types';
@@ -16,6 +17,13 @@ export const toggleHeader = showHeader => (dispatch) => {
   dispatch({
     type: TOGGLE_HEADER,
     payload: showHeader,
+  });
+};
+
+export const toggleLoader = showLoader => (dispatch) => {
+  dispatch({
+    type: TOGGLE_LOADER,
+    payload: showLoader,
   });
 };
 
@@ -37,6 +45,7 @@ export const changeThemeColor = themeColor => (dispatch) => {
 export default {
   toggleSideMenu,
   toggleHeader,
+  toggleLoader,
   toggleNerve,
   changeThemeColor,
 };
